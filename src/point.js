eb53c296e8f73b0b2c92954d05e3b95c65937176
Bruno Windels <bruno.windels@gmail.com>
2015-05-31 00:41:15 +0200

    add tostring method

--- src/point.js
+++ src/point.js
@@ -9,6 +9,12 @@ function Point(x, y) {
 	this.multiply = function(n) {
 		return new Point(this.x * n, this.y * n);
 	}
+	this.toString = function() {
+		return 'x:'+this.x+',y:'+this.y;
+	}
+	this.equals = function(p) {
+		return p.x === this.x && p.y === this.y;
+	}
 }
 
 module.exports = Point;
