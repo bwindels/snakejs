a685b4d8502cde3d540b2ab779519b1eae5b063e
Bruno Windels <bruno.windels@gmail.com>
2015-05-30 23:46:05 +0200

    fix issues with segment and add iteratePoints method + unit test

--- src/segment.js
+++ src/segment.js
@@ -24,17 +24,35 @@ function Segment(head, tail) {
 
 	this.length = function() {
 		if(this.head.x === this.tail.x) {
-			return (this.head.y - this.tail.y) + 1;
+			return Math.abs(this.head.y - this.tail.y);
 		}
 		if(this.head.y === this.tail.y) {
-			return (this.head.x - this.tail.x) + 1;
+			return Math.abs(this.head.x - this.tail.x);
 		}
 		throw new Error('segment should be axis aligned');
 	}
 
-	this.shrink = function(n) {
-		this.tail = this.tail.translate(this.axis().multiply(n));
+	this.shrink = function() {
+		this.tail = this.tail.translate(this.axis());
+		return this.tail;
 	}
+
+	this.grow = function() {
+		this.head = this.head.translate(this.axis());
+		return this.head;
+	}
+
+	this.iteratePoints = function(callback) {
+		var axis = this.axis();
+		var length = this.length();
+		var i = 1;
+
+		while(i <= length) {
+			callback(this.tail.translate(axis.multiply(i)));
+			++i;
+		}
+	}
+
 }
 
 module.exports = Segment;
--- test/segment.test.js
+++ test/segment.test.js
@@ -1,12 +1,101 @@
 var Point = require('../src/point.js');
 var Segment = require('../src/segment.js');
 
+function collectPoints(segment) {
+	var points = [];
+	segment.iteratePoints(function(p) {
+		points.push({x: p.x, y: p.y});
+	});
+	return points;
+}
+
 module.exports = {
 	'test shrink': function(test) {
 		var s = new Segment(new Point(1,4), new Point(1,0));
-		test.strictEqual(s.length(), 5);
-		s.shrink(2);
+		test.strictEqual(s.length(), 4);
+		
+		var clearedCell = s.shrink();
+		test.strictEqual(clearedCell.x, 1);
+		test.strictEqual(clearedCell.y, 1);
+
 		test.strictEqual(s.length(), 3);
+		test.deepEqual(collectPoints(s), [
+			{x: 1, y: 2},
+			{x: 1, y: 3},
+			{x: 1, y: 4}
+		]);
 		test.done();
-	}	
+	},
+
+	'test grow': function(test) {
+		var s = new Segment(new Point(1,4), new Point(1,2));
+		test.strictEqual(s.length(), 2);
+
+		var filledCell = s.grow();
+		test.strictEqual(filledCell.x, 1);
+		test.strictEqual(filledCell.y, 5);
+
+		test.strictEqual(s.length(), 3);
+		test.deepEqual(collectPoints(s), [
+			{x: 1, y: 3},
+			{x: 1, y: 4},
+			{x: 1, y: 5}
+		]);
+		test.done();
+	},
+
+	'test axis': function(test) {
+		var s, axis;
+		
+		s = new Segment(new Point(1,4), new Point(1,0));
+		axis = s.axis();
+		test.strictEqual(axis.x, 0);
+		test.strictEqual(axis.y, 1);
+
+		s = new Segment(new Point(1,4), new Point(1,5));
+		axis = s.axis();
+		test.strictEqual(axis.x, 0);
+		test.strictEqual(axis.y, -1);
+		
+		s = new Segment(new Point(100,5), new Point(7,5));
+		axis = s.axis();
+		test.strictEqual(axis.x, 1);
+		test.strictEqual(axis.y, 0);
+		
+		s = new Segment(new Point(-100,5), new Point(9,5));
+		axis = s.axis();
+		test.strictEqual(axis.x, -1);
+		test.strictEqual(axis.y, 0);
+
+		s = new Segment(new Point(0,0), new Point(1,1));
+		test.throws(function() {
+			s.axis();
+		})
+
+		test.done();
+	},
+
+	'test iteratePoints with tail smaller than head': function(test) {
+		var points = [];
+		var s = new Segment(new Point(1,4), new Point(1,0));
+		test.deepEqual(collectPoints(s), [
+			{x: 1, y: 1},
+			{x: 1, y: 2},
+			{x: 1, y: 3},
+			{x: 1, y: 4}
+		]);
+		test.done();
+	},
+
+	'test iteratePoints with tail bigger than head': function(test) {
+		var points = [];
+		var s = new Segment(new Point(1,4), new Point(1,8));
+		test.deepEqual(collectPoints(s), [
+			{x: 1, y: 7},
+			{x: 1, y: 6},
+			{x: 1, y: 5},
+			{x: 1, y: 4}
+		]);
+		test.done();
+	}
 }
